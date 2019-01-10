549585ed7dbc18808943e8e30de2a0d8bb57580f
Sam Thomson <samt@samt.st>
2019-01-10 10:19:17 +0700

    testing in

---
+++ test/helper.spec.ts
@@ -0,0 +1,14 @@
+// DEFINE MOCKS
+
+import { expect } from 'chai'
+// import {
+//     parseGPXData,
+// } from '../src/renderer/lib'
+// import { GPXData, GPXPoint } from '../src/declarations'
+
+describe('helper', () => {
+    it('should parse GPX data', () => {
+        expect(10).to.equal(10)
+        expect(10).to.equal(11)
+    })
+})
